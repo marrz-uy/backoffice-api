f688c6add6c153351b52a2b630ec31928c626b93
unknown <Javi>
2022-08-28 13:10:03 -0300

    Guardado

--- tests/Feature/PuntoDeInteresTest.php
+++ tests/Feature/PuntoDeInteresTest.php
@@ -15,7 +15,7 @@ class PuntoDeInteresTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_Pagination()
+    public function test_Pagination_Punto_De_Interes()
     {
         $response = $this->get('/api/PuntosInteres/PuntosDeInteres');
         $response->assertJsonCount(10,"data");
@@ -23,9 +23,18 @@ class PuntoDeInteresTest extends TestCase
         $response->assertJsonStructure(["last_page_url"]);
         $response->assertJsonStructure(["current_page"]);
 
+    }
+    public function test_Pagination_Servicio_Esencial()
+    {
+        $response = $this->get('/api/PuntosInteres/servicios_esenciales');
+        $response->assertJsonCount(10,"data");
+        $response->assertJsonStructure(["next_page_url"]);
+        $response->assertJsonStructure(["last_page_url"]);
+        $response->assertJsonStructure(["current_page"]);
 
     }
-    public function test_Alta_De_Servicio(){
+    
+    public function test_Alta_De_Servicio_Esencial(){
         $response = $this->withHeaders([
             'content-type' => 'application/json',
         ])->postJson('/api/PuntosInteres', [
@@ -37,28 +46,23 @@ class PuntoDeInteresTest extends TestCase
             'Horario'=>null,
             'Descripcion'=>'Farmaciadeltest',
             'Imagen'=>null,
-            'Op'=>'ServicioEsencial',
-            'tipo'=>'Farmacias'
+            "InformacionDetalladaPuntoDeInteres"=>"{\"Tipo\":\"Seccionales\",\"Op\":\"ServicioEsencial\"}"
         ]);
-        $response=$this->get('/')->assertStatus(200);
-       //$response->assertStatus(200);
+            $response->assertStatus(200);
     }
-    public function test_Baja_De_Servicio(){
+    
+    public function test_Baja_De_Punto_De_Interes(){
         $response = $this->withHeaders([
             'content-type' => 'application/json',
-        ])->postJson('/api/PuntosInteres', [
-            'Nombre'                => 'FarmaciaTEST',
-            'Departamento'             => 'Montevideo',
-            'Ciudad' => 'Montevideo',
-            'Direccion' => '18 de Julio 3014',
-            'Contacto'=>'29025010',
-            'Horario'=>null,
-            'Descripcion'=>'Farmaciadeltest',
-            'Imagen'=>null,
-            'Op'=>'ServicioEsencial',
-            'tipo'=>'Farmacias'
-        ]);
-        $response=$this->get('/')->assertStatus(200);
-       //$response->assertStatus(200);
+        ])->delete('/api/PuntosInteres/47/PuntosDeInteres', []);
+        
+       $response->assertStatus(200);
+    }
+    public function test_Baja_De_Servicio_Esencial(){
+        $response = $this->withHeaders([
+            'content-type' => 'application/json',
+        ])->delete('/api/PuntosInteres/21/servicios_esenciales',[]);
+        
+       $response->assertStatus(200);
     }
 }
